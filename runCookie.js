08a5c965ccbc5889c9b3ab2d2401b954c7a6251a
feleecor <102253095+feleecor@users.noreply.github.com>
2022-03-23 20:35:34 +0200

    Create runCookie.js

---
+++ runCookie.js
@@ -0,0 +1,81 @@
+var ready = function(fn) {
+  if (document.readyState != 'loading') {
+    fn();
+  } else if (document.addEventListener) {
+    document.addEventListener('DOMContentLoaded', fn);
+  } else {
+    document.attachEvent('onreadystatechange', function() {
+      if (document.readyState != 'loading') {
+        fn();
+      }
+    });
+  }
+
+  return;
+};
+
+/**
+ * If loaded DOM elements
+ **/
+ready(function() {
+  var cookiebar = new Cookiebar({
+    debug: 1,
+    content: {
+      description: "Acest site folosește cookies. Prin continuarea utilizării site-ului, vă exprimați acordul cu privire la modulele noastre cookie.",
+      link: "",
+      href: "#",
+      button: "Am inteles!",
+    },
+    onAccept: function () {
+    }
+  });
+
+  
+
+
+let xhr = new XMLHttpRequest();
+xhr.open('get', 'http://balnak.dreamweb.ro/cookies/policy.pol');
+xhr.send();
+
+xhr.onload = function() {
+	var myDiv = document.createElement("div");
+
+//Set its unique ID.
+myDiv.id = 'div_id';
+
+//Add your content to the DIV
+myDiv.innerHTML = xhr.response;
+
+//Finally, append the element to the HTML body
+document.body.appendChild(myDiv);
+
+const openEl = document.getElementsByClassName("open-modal")[0];
+const closeEl = document.getElementsByClassName("close-modal")[0];
+const isVisible = "is-visible";
+
+openEl.addEventListener("click", function(e) {
+    document.getElementById('modal1').classList.add(isVisible);
+}, false);
+
+closeEl.addEventListener("click", function(e) {
+    document.getElementById('modal1').classList.remove(isVisible);
+}, false);
+document.addEventListener("click", e => {
+  if (e.target == document.querySelector(".modal.is-visible")) {
+    document.querySelector(".modal.is-visible").classList.remove(isVisible);
+  }
+});
+
+document.addEventListener("keyup", e => {
+  // if we press the ESC
+  if (e.key == "Escape" && document.querySelector(".modal.is-visible")) {
+    document.querySelector(".modal.is-visible").classList.remove(isVisible);
+  }
+});
+};
+
+
+});
+
+
+
